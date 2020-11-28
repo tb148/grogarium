1993327bd6ab4bc83e264ce39bb2a86e4a38cc4d
Tony Brown <tb148@tuta.io>
2020-11-28 02:47:32 +0000

    fix: remove the embed

--- src/fun.py
+++ src/fun.py
@@ -82,7 +82,6 @@ class Fun(commands.Cog, name=config["fun"]["name"]):
             "{} :negative_squared_cross_mark: I lost the game. (https://en.wikipedia.org/wiki/The_Game_(mind_game))".format(
                 ctx.author.mention,
             ),
-            embed=discord.Embed(url="https://xkcd.com/391/"),
         )
 
 
