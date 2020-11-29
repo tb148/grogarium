d5e2fde18a7b92060f2828f10e4c3cbe36aaa312
Tony Brown <tb148@tuta.io>
2020-11-29 07:52:46 +0000

    style: change the embed

--- src/fun.py
+++ src/fun.py
@@ -78,12 +78,6 @@ class Fun(commands.Cog, name=config["fun"]["name"]):
     )
     async def game(self, ctx):
         """Plays the game."""
-        embed = discord.Embed(
-            title="xkcd: Anti-Mindvirus",
-            description="I'm as surprised as you!  I didn't think it was possible.",
-            url="https://xkcd.com/391/",
-        )
-        embed.set_image(url="https://imgs.xkcd.com/comics/anti_mind_virus.png")
         if random.random() > config["game"]["win-chance"]:
             await ctx.send(
                 "{} :negative_squared_cross_mark: I lost the game. (https://en.wikipedia.org/wiki/The_Game_(mind_game))".format(
@@ -91,7 +85,11 @@ class Fun(commands.Cog, name=config["fun"]["name"]):
                 ),
             )
         else:
-            await ctx.send(embed=embed)
+            await ctx.send(embed=discord.Embed(
+              title="xkcd: Anti-Mindvirus",
+              description="I'm as surprised as you!  I didn't think it was possible.",
+              url="https://xkcd.com/391/",
+            ).set_image(url="https://imgs.xkcd.com/comics/anti_mind_virus.png"))
 
 
 def setup(bot):
